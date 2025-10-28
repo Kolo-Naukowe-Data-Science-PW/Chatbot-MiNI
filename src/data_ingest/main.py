1bc1f6c8ee6129766e7045e5c401c3c4a2defa52
PiotrDS <01180852@pw.edu.pl>
2025-10-28 21:50:20 +0100

    Autoformat: Black fixed code style

--- src/data_ingest/main.py
+++ src/data_ingest/main.py
@@ -7,8 +7,11 @@ from data_ingest.modules.embedder import Embedder
 from data_ingest.modules.extractor import extract_text
 from data_ingest.modules.vector_db import save_to_vector_db
 from scraper.main import STORAGE_DIR
+from utils.paths import get_data_dir
 
 logging.basicConfig(level=logging.INFO)
+
+DATABASE_PATH = get_data_dir("temp_database")
 
 
 def ingest_documents():
@@ -24,7 +27,13 @@ def ingest_documents():
     all_files = [os.path.join(STORAGE_DIR, f) for f in os.listdir(STORAGE_DIR)]
     logging.info("Found %d files to ingest.", len(all_files))
 
-    for file_path in all_files:
+    number_of_files = len(all_files)
+
+    embeddings = [None for _ in range(number_of_files)]
+    chunks = [None for _ in range(number_of_files)]
+    urls = [None for _ in range(number_of_files)]
+
+    for index, file_path in enumerate(all_files):
 
         logging.info("Processing file: %s", file_path)
         with open(file_path, encoding="utf-8") as f:
@@ -33,14 +42,15 @@ def ingest_documents():
 
         cleaned_text = clean_html(raw_content)
 
-        chunks = chunk_text(cleaned_text)
+        chunk = chunk_text(cleaned_text)
 
-        embeddings = embedder.generate_embeddings(chunks)
+        embedding = embedder.generate_embeddings(chunks)
 
-        for chunk, embedding in zip(chunks, embeddings, strict=False):
-            save_to_vector_db(
-                text_chunk=chunk, embedding=embedding, source_url=url_line
-            )
+        chunks[index] = chunk
+        embeddings[index] = embedding
+        urls[index] = url_line
+
+    save_to_vector_db(chunks, embeddings, urls, DATABASE_PATH)
 
     logging.info("Ingestion complete for all documents.")
 
