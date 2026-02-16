b1b13ecba04625b1278e165562b63aa0f178e6f9
Wiktoria Grodzka <wiktoriagrodzka@Wiktorias-MacBook-Air.local>
2026-02-16 01:22:36 +0100

    Change in vite.config.js + deploy only frontend.

--- src/frontend/frontend_feb/vite.config.js
+++ src/frontend/frontend_feb/vite.config.js
@@ -4,16 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // # dodane: proxy dla połączenia frontend -> API w Dockerze
   server: {
-  // # dodane: dozwolone hosty dla Vite na VM
-  allowedHosts: ["chatbotknds.mini.pw.edu.pl"],
-  proxy: {
-    "/api": {
-      target: "http://api:8000",
-      changeOrigin: true,
+    allowedHosts: ["chatbotknds.mini.pw.edu.pl"],
+    proxy: {
+      "/api": {
+        target: "http://api:8000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
+});
 
-})
