775cfdf072c248902a4e1051744fa3cbb22f0296
Tymon Tumialis <tymontumialis@gmail.com>
2026-03-21 19:27:55 +0100

    .

--- src/chat_bot_auto_evaluation/tests/evaluation_test.py
+++ src/chat_bot_auto_evaluation/tests/evaluation_test.py
@@ -1,8 +1,13 @@
-# from src.chat_bot_auto_evaluation.evaluate import *
-# from src.chat_bot_auto_evaluation.metrics.metrics import *
+import numpy as np
 import pandas as pd
+import torch
 
-from src.chat_bot_auto_evaluation.read_data import read_data_from_csv
+from chat_bot_auto_evaluation.metrics import Metrics
+from chat_bot_auto_evaluation.prepare_data import (
+    convert_data_frame_to_string,
+    get_data,
+    read_data_from_csv,
+)
 
 FILE_PATH = "src/chat_bot_auto_evaluation/tests/test.csv"
 COLUMNS = ["a", "b"]
@@ -14,3 +19,35 @@ def test_read_data():
     assert type(read_data_from_csv("no_file.csv", COLUMNS)) is Exception
     assert type(read_data_from_csv(FILE_PATH[:-3], COLUMNS)) is Exception
     assert type(read_data_from_csv(FILE_PATH, COLUMNS[0])) is Exception
+
+
+def test_convert_data_frame_to_string():
+    df = read_data_from_csv(FILE_PATH, COLUMNS)
+    assert type(convert_data_frame_to_string(df)) is np.ndarray
+    assert len(convert_data_frame_to_string(df)) == 2
+
+
+def test_get_data():
+    assert type(get_data(FILE_PATH, COLUMNS)) is np.ndarray
+    assert len(get_data(FILE_PATH, COLUMNS)) == 2
+
+
+# testing metrics class and methods
+
+candidates = ["Pogoda jest dzisiaj naprawdę piękna."]
+references = ["Dzisiaj na zewnątrz jest cudowny dzień."]
+
+
+class TestMetrics:
+
+    def test_init(self):
+        object = Metrics(candidates, references)
+        assert object.candidates == candidates
+        assert object.references == references
+
+    def test_bert_score(self):
+        object = Metrics(candidates, references)
+        P, R, F1 = object.bert_score()
+        assert type(P) is torch.Tensor
+        assert type(R) is torch.Tensor
+        assert type(F1) is torch.Tensor
